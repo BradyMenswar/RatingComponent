b03bc9968e1e919b4433b85fce948316b288f946
BradyMenswar <bradymenswar@gmail.com>
2022-05-03 09:27:57 -0400

    Finished adding JS and styling component

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-let submitButton = document.querySelector(".submit-button");
+let rating = 1;
+
 document.addEventListener('click', e => {
-    const isRatingButton = e.target.matches("[data-rating]")
+    const isRatingButton = e.target.matches("[data-rating]");
+    const isSubmitButton = e.target.matches("[data-submit]");
 
-    if(!isRatingButton) return;
+    if(!isRatingButton && !isSubmitButton) return;
     let currentRating;
-    currentRating = e.target;
-    e.target.classList.toggle('active');
 
+    if(isRatingButton) {
+        currentRating = e.target;
+        e.target.classList.toggle('active');
+        rating = currentRating.textContent;
+    }
+    
+    if(isSubmitButton) {
+        thankYou = document.getElementById("thank-you-container");
+        ratings = document.getElementById("rating-container")
+        ratings.style.display = "none";
+        thankYou.style.display = "flex";
+
+        let caption = document.querySelector("#rating-caption");
+        caption.textContent = "You selected " + rating + " out of 5.";
+    }
 
     document.querySelectorAll("[data-rating].active").forEach(rating => {
 
         if(rating === currentRating) return;
         rating.classList.remove('active');
     })
-})
-submitButton.addEventListener('click', e => {
-
 })
